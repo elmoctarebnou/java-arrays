40b7714b867d45b7374e93309903b2c16413e559
El Moctar Ebnou <ebnoumoctar@gmail.com>
2020-11-18 23:34:13 -0600

    java arrays

---
+++ src/CovidStats.java
@@ -0,0 +1,2 @@
+public class CovidStats {
+}
---
+++ src/TryItOut.java
@@ -0,0 +1,22 @@
+public class TryItOut {
+    public static void main(String[] args){
+        int[] numbers;
+        numbers = new int[8];
+        System.out.println(numbers[0]);
+        String[] names;
+        names = new String[50];
+        boolean[] flags;
+        flags = new boolean[560];
+        numbers[0] = 19;
+        numbers[1] = 12;
+        numbers[2] = 72;
+        numbers[3] = 68;
+        numbers[4] = 44;
+        numbers[5] = 92;
+        numbers[6] = 54;
+        numbers[7] = 36;
+        int sum = numbers[2] + numbers[4];
+        System.out.println(sum);
+        System.out.println(numbers.length);
+    }
+}
